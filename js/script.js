850a95d148fbb908fb5d4248eb4a58e0a14bdd05
Deepak00001 <deepakgupt001@gmail.com>
2020-09-19 01:01:37 +0530

    Lottery and Footer section

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function random (){
+    document.getElementById("number").innerHTML=Math.floor(Math.random()*11)+10;
+}
+/*Enterbtn.addEventListener('number'() =>{
+    if(number.value.length >0){
+        Enterbtn.removeAttribute('abled');
+    }else{
+        loginbtn.setAttribute('abled', 'abled')
+    }
+})*/
+
+/*function random (){
+    document.write(Math.floor(Math.random() *20))
+}*/
+/*function random (){
+    var random = Math.floor(Math.random() *10) +10;
+    console.log(random);
+}*/
+var x=getElementById
