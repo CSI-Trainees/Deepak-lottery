27cc4d16a9ec32ad0d3b5dc8b4ac3eedcf1f5de1
Deepak00001 <deepakgupt001@gmail.com>
2020-09-20 15:29:53 +0530

    ROUND 2 & review modified

--- js/script.js
+++ js/script.js
@@ -16,4 +16,14 @@ function random (){
     var random = Math.floor(Math.random() *10) +10;
     console.log(random);
 }*/
-var x=getElementById
+function checklottery(event) {
+    var num = document.getElementById("text").value;
+    if(num==10||num==15||num==20){
+        document.getElementById("result").style.display = "block";
+        document.getElementById("winner").style.display = "block";
+    }
+    else if(num>10&&num<15||num>15&&num<20) {
+        document.getElementById("result").style.display = "block";
+        document.getElementById("loose").style.display = "block";
+    }
+}
