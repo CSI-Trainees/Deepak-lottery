9bec9c8ec31593575fbe2e82f73fb44deac27c02
Deepak00001 <deepakgupt001@gmail.com>
2020-09-21 23:47:38 +0530

    Button locked and some review

--- js/script.js
+++ js/script.js
@@ -1,29 +1,17 @@
 function random (){
     document.getElementById("number").innerHTML=Math.floor(Math.random()*11)+10;
+    document.getElementById("Enterbtn").style.display = "none";
 }
-/*Enterbtn.addEventListener('number'() =>{
-    if(number.value.length >0){
-        Enterbtn.removeAttribute('abled');
-    }else{
-        loginbtn.setAttribute('abled', 'abled')
-    }
-})*/
-
-/*function random (){
-    document.write(Math.floor(Math.random() *20))
-}*/
-/*function random (){
-    var random = Math.floor(Math.random() *10) +10;
-    console.log(random);
-}*/
-function checklottery(event) {
+function checklottery() {
     var num = document.getElementById("text").value;
     if(num==10||num==15||num==20){
         document.getElementById("result").style.display = "block";
         document.getElementById("winner").style.display = "block";
+        document.getElementById("submitvalue").style.display = "none";
     }
     else if(num>10&&num<15||num>15&&num<20) {
         document.getElementById("result").style.display = "block";
         document.getElementById("loose").style.display = "block";
+        document.getElementById("submitvalue").style.display = "none";
     }
 }
